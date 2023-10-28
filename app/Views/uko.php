2ff1386712b430506ae1b720d96901cd555afcab
OsmondHaylim <osmond20001@mail.unpad.ac.id>
2023-10-28 18:18:55 +0700

    navbar

--- app/Views/uko.php
+++ app/Views/uko.php
@@ -2,33 +2,38 @@
 <html lang="en">
 <head>
     <title>Welcome to CodeIgniter 4!</title>
+    <link rel="stylesheet" href="/Asset/style.css">
 </head>
 <body>
-    <div>
-        
-    </div>
-    <table>
-        <thead>
-            <tr>
-                <th>KD UKO</th>
-                <th>Nama KC</th>
-                <th>Jumlah PTP</th>
-                <th>Persen PTP</th>
-                <th>OS PTP</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach($uko as $row) : ?>
+    <div class="container">
+        <div class="navbar">
+            <a href="#" class="nav_home">Home</a>
+            <a href="#" class="logo">Point to Point Monitoring</a>
+        </div>
+        <table>
+            <thead>
                 <tr>
-                    <td><?= $row->KD_UKO; ?></td>
-                    <td><?= $row->UKO_Name; ?></td>
-                    <td><?= $row->PTP_Amount; ?></td>
-                    <td><?= $row->PTP_Percentage; ?></td>
-                    <td><?= $row->OS_PTP; ?></td>
+                    <th>KD UKO</th>
+                    <th>Nama KC</th>
+                    <th>Jumlah PTP</th>
+                    <th>Persen PTP</th>
+                    <th>OS PTP</th>
                 </tr>
-                
-            <?php endforeach;?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php foreach($uko as $row) : ?>
+                    <tr>
+                        <td><?= $row->KD_UKO; ?></td>
+                        <td><?= $row->UKO_Name; ?></td>
+                        <td><?= $row->PTP_Amount; ?></td>
+                        <td><?= $row->PTP_Percentage; ?></td>
+                        <td><?= $row->OS_PTP; ?></td>
+                    </tr>
+                    
+                <?php endforeach;?>
+            </tbody>
+        </table>
+    </div>
+    
 
 </body>
